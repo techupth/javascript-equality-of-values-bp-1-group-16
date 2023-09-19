ab44755ac7c74a36b79a6aa7c95a48889a86df04
Palathip <windeesel365@gmail.com>
2023-09-20 00:20:21 +0700

    feat(ex1 2 3): created and done

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false;    // true  falsy values
+null == undefined;  //true   falsy values
+10 == "10";  //true
+[10] == 10;  //false  list(array) != integer
+[1,2] == "1,2";  //false  list(array) != string
--- ex-3.js
+++ ex-3.js
@@ -1,5 +1,16 @@
 function isPlainObject(value) {
   // Start coding here
+  if (typeof value === "object" && !Array.isArray(value) && value !== null) {
+    return true;
+  } else if (typeof value === "object" && (Array.isArray(value) || value === null)) {
+    return false;    
+  //} else if (Array.isArray(value)) {
+  //  return false;
+  //} else if (value === null) {
+  //  return false;
+  } else {
+    return "This is not an object";
+  }
 }
 
 // Example case
@@ -11,3 +22,12 @@ let result3 = isPlainObject(["apples", "oranges"]);
 console.log(result1); //true
 console.log(result2); // false
 console.log(result3); // false
+
+
+//รับ Parameter 1 ตัว ซึ่งมี Value เป็น Data Type อะไรก็ได้ (เช่น String, Number, Object, Null, Undefined, Etc)
+//โดย Function นี้เอาไว้ตรวจสอบ Data Type ของ Parameter ที่ส่งเข้ามาว่า Data Type เป็น Object จริงๆ ไม่ใช่ Null หรือ Array หรืออื่น ๆ
+//ผลลัพธ์ที่ Return ออกมาจาก Function จะมี Value เป็น Boolean
+//ถ้าผลลัพธ์มี Data Type เป็น Object ผลลัพธ์จะเป็น true
+//ถ้าผลลัพธ์มี Data Type เป็น Array ผลลัพธ์จะเป็น false
+//ถ้าผลลัพธ์มี Data Type เป็น Null ผลลัพธ์จะเป็น false
+//กรณีที่ผลลัพธ์ไม่เป็นทั้ง Object, Array หรือ Null ให้ Return ข้อความว่า “This is not an object”
